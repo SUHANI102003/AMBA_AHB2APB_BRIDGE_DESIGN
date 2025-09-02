7b8e9c54217ce49c7effae511695c54135015e55
SUHANI JAIN <jsuhani000710@gmail.com>
2025-09-02 15:17:51 +0530

    Update Bridge_Top.v

--- RTL/Bridge_Top.v
+++ RTL/Bridge_Top.v
@@ -1,43 +1,69 @@
-module Bridge_Top(Hclk,Hresetn,Hwrite,Hreadyin,Hreadyout,Hwdata,Haddr,Htrans,Prdata,Penable,Pwrite,Pselx,Paddr,Pwdata,Hreadyout,Hresp,Hrdata);
+module Bridge_Top(
+    input  Hclk,
+    input  Hresetn,
+    input  Hwrite,
+    input  Hreadyin,
+    input  [31:0] Hwdata,
+    input  [31:0] Haddr,
+    input  [31:0] Prdata,
+    input  [1:0]  Htrans,
+    output Penable,
+    output Pwrite,
+    output Hreadyout,
+    output [1:0]  Hresp, 
+    output [2:0]  Pselx,
+    output [31:0] Paddr,
+    output [31:0] Pwdata,
+    output [31:0] Hrdata
+);
 
-input Hclk,Hresetn,Hwrite,Hreadyin;
-input [31:0] Hwdata,Haddr,Prdata;
-input[1:0] Htrans;
-output Penable,Pwrite,Hreadyout;
-output [1:0] Hresp; 
-output [2:0] Pselx;
-output [31:0] Paddr,Pwdata;
-output [31:0] Hrdata;
-
-//INTERMEDIATE SIGNALS
-
+// INTERMEDIATE SIGNALS
 wire valid;
-wire [31:0] Haddr1,Haddr2,Hwdata1,Hwdata2;
+wire [31:0] Haddr1, Haddr2, Hwdata1, Hwdata2;
 wire Hwritereg;
 wire [2:0] tempselx;
 
 // MODULE INSTANTIATIONS
-
 AHB_SLAVE_interface AHBSlave (
-	Hclk,
-	Hresetn,
-	Hwrite,
-	Hreadyin,
-	Htrans,
-	Haddr,
-	Hwdata,
-	Hresp,
-	Hrdata,
-	valid,
-	Haddr1,
-	Haddr2,
-	Hwdata1,
-	Hwdata2,
-	Hwritereg,
-	Hwritereg1,
-	tempselx,
-	Prdata);
+    .Hclk(Hclk),
+    .Hresetn(Hresetn),
+    .Hwrite(Hwrite),
+    .Hreadyin(Hreadyin),
+    .Htrans(Htrans),
+    .Haddr(Haddr),
+    .Hwdata(Hwdata),
+    .Hresp(Hresp),
+    .Hrdata(Hrdata),
+    .valid(valid),
+    .Haddr1(Haddr1),
+    .Haddr2(Haddr2),
+    .Hwdata1(Hwdata1),
+    .Hwdata2(Hwdata2),
+    .Hwritereg(Hwritereg),
+    .tempselx(tempselx),
+    .Prdata(Prdata)
+);
 
-APB_controller APBControl ( Hclk,Hresetn,valid,Haddr1,Haddr2,Hwdata1,Hwdata2,Prdata,Hwrite,Haddr,Hwdata,Hwritereg,tempselx,Pwrite,Penable,Pselx,Paddr,Pwdata,Hreadyout);
+APB_controller APBControl (
+    .Hclk(Hclk),
+    .Hresetn(Hresetn),
+    .valid(valid),
+    .Haddr1(Haddr1),
+    .Haddr2(Haddr2),
+    .Hwdata1(Hwdata1),
+    .Hwdata2(Hwdata2),
+    .Prdata(Prdata),
+    .Hwrite(Hwrite),
+    .Haddr(Haddr),
+    .Hwdata(Hwdata),
+    .Hwritereg(Hwritereg),
+    .tempselx(tempselx),
+    .Pwrite(Pwrite),
+    .Penable(Penable),
+    .Pselx(Pselx),
+    .Paddr(Paddr),
+    .Pwdata(Pwdata),
+    .Hreadyout(Hreadyout)
+);
 
 endmodule
